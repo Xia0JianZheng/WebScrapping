ce1e1e42bc915ebd97a3165734c136b14ca19fd7
Xia0JianZheng <zhengx@elpuig.xeill.net>
2022-11-21 17:55:32 +0100

    inicial commit

---
+++ src/main/java/Personaje.java
@@ -0,0 +1,47 @@
+public class Personaje {
+    String nombre;
+    int Rareza;
+    String Historia;
+    String Elemento;
+    String Region;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRareza() {
+        return Rareza;
+    }
+
+    public void setRareza(int rareza) {
+        Rareza = rareza;
+    }
+
+    public String getHistoria() {
+        return Historia;
+    }
+
+    public void setHistoria(String historia) {
+        Historia = historia;
+    }
+
+    public String getElemento() {
+        return Elemento;
+    }
+
+    public void setElemento(String elemento) {
+        Elemento = elemento;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String region) {
+        Region = region;
+    }
+}
